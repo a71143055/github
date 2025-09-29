db7549a9cb2de9d5fcc3da01ea1980d00f2bd36b
a71143055 <a71143055@gmail.com>
2025-09-29 23:00:58 +0900

    github

---
+++ src/test/java/kr/ac/kopo/jeong/github/GithubApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.jeong.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
